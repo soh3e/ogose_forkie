22b675bbb54d746f6a68c910840cdfe0f37e1639
Cybertron51 <90730846+Cybertron51@users.noreply.github.com>
2021-12-15 09:22:02 -0800

    Create shellcheck.py

---
+++ shellcheck.py
@@ -0,0 +1,9 @@
+import pwd, sys
+
+user_arr = []
+with open('authusers.txt') as my_file:
+    for line in my_file:
+        user_arr.append(line)
+        
+for i in range(len(user_arr)):
+    (user_arr[i])
