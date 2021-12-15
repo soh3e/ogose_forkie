35efd43db71ff3236f09f84571ae9e050b778697
Cybertron51 <90730846+Cybertron51@users.noreply.github.com>
2021-12-15 14:51:11 -0800

    Create shellcheck.py

--- shellcheck.py
+++ shellcheck.py
@@ -2,9 +2,9 @@ import pwd, sys
 
 user_arr = []
 with open('authusers.txt') as my_file:
-    for line in my_file:
-        user_arr.append(line)
+    user_arr=my_file.read().splitlines()
         
 for i in range(len(user_arr)):
-    user_arr[i] = pwd.getpwnam(user_arr)
-    print((user_arr[i].pw_name + ": " + user_arr[i].pw_shell)
+    print(user_arr)
+    #user_arr[i] = pwd.getpwnam(user_arr[i])
+    #print((user_arr[i].pw_name + ": " + user_arr[i].pw_shell)
