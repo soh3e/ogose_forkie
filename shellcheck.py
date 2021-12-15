eb4d16cc658cd7a4c03f5590e3cc7f9cfe84aaf0
Cybertron51 <90730846+Cybertron51@users.noreply.github.com>
2021-12-15 09:46:44 -0800

    Update shellcheck.py

--- shellcheck.py
+++ shellcheck.py
@@ -8,5 +8,3 @@ with open('authusers.txt') as my_file:
 for i in range(len(user_arr)):
     user_arr[i] = pwd.getpwnam(user_arr)
     print((user_arr[i].pw_name + ": " + user_arr[i].pw_shell)
-          
-          
